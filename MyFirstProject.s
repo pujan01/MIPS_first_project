5810fa42451d2d1a8b4ba32907fa3f1cd57fd759
pujan01 <thapapujan2@gmail.com>
2018-10-15 16:26:02 -0400

    I changed the code to display @02
    To print @, i used the ASCOO value, and I printed the number using subtraction and addition. 0 = 64 +(-64)
    lastly, adding 2 and printing

--- MyFirstProject.s
+++ MyFirstProject.s
@@ -1,8 +1,7 @@
 # PROGRAM: My First Project!
 
-# Pujan Thapa
-# @02842703
-
+#   @02842703
+#   Thapa, Pujan
 
 
 .data 			# Data declaration section 
@@ -13,12 +12,20 @@ main:			# Start of code section
 li $v0, 11		# system call code for printing character = 11
 
 
-li $a0, 'P'		# get the first letter of my name
+li $a0, 64  	# get the ascii code for @ symbol
 			    # to be printed into $a0
 
 syscall			# call operating system to perform operation
 			    # in $v0
 			    # syscall takes its arguments from $a0, $a1, ...
+
+li $v0, 1       # system call code for printing integer = 1
+
+addi $a0, $a0, -64
+syscall
+
+addi $a0, $a0, 2
+syscall
 
 
 
